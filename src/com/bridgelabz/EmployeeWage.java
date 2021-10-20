26c61541dd393873fa51a514b27de19637874682
GeetaSMath <geetasmath@gmail.com>
2021-10-19 22:57:56 -0600

    Added: calculate daily wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,32 +1,19 @@
-package com.bridgelabz;
-
 import java.util.Random;
 
-public class EmployeeWage {
+public class EmployeeWage{
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int Wage_HR = 20;
         int Day_HRs = 8;
-        int Part_HRs = 8;
         Random random = new Random();
         int r = random.nextInt(2);
         if(r == 1){
             System.out.println("Employee Present");
             System.out.println("DayWage = " + Wage_HR * Day_HRs);
         }
-        else if(r == 2){
-            System.out.println("Employee Present");
-            System.out.println("DayWage with Part Time = " + Wage_HR * (Day_HRs + Part_HRs));
-        }
-
         else
-
             System.out.println("Employee Absent");
 
-
-
-
     }
-
 }
