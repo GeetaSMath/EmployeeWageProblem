193f1418d217288eec7c530fd171aef7063f3e07
GeetaSMath <geetasmath@gmail.com>
2021-10-19 23:26:35 -0600

    Added: added part time employee wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,19 +1,25 @@
 import java.util.Random;
 
-public class EmployeeWage{
+public class EmployeeWage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
         int Wage_HR = 20;
         int Day_HRs = 8;
+        int Part_HRs = 8;
+
         Random random = new Random();
-        int r = random.nextInt(2);
+        int r = random.nextInt(3);
         if(r == 1){
             System.out.println("Employee Present");
             System.out.println("DayWage = " + Wage_HR * Day_HRs);
         }
+        else if(r == 2){
+            System.out.println("Employee Present");
+            System.out.println("DayWage with Part Time = " + Wage_HR * (Day_HRs + Part_HRs));
+        }
+
         else
             System.out.println("Employee Absent");
-
     }
 }
