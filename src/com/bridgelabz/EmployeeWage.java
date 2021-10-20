7f47cdcbaafef53e1b9d4801434851d062d2008e
GeetaSMath <geetasmath@gmail.com>
2021-10-19 23:40:10 -0600

    Added: employee wage calculate for month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-
 public class EmployeeWage{
 
     public static void main(String[] args) {
@@ -7,20 +6,29 @@ public class EmployeeWage{
         int Wage_HR = 20;
         int Day_HRs = 8;
         int Part_HRs = 8;
+        int NumDays = 20;
+        double daywage = 0;
+        double monthwage = 0;
 
         Random random = new Random();
-        int r = random.nextInt(2);
-        switch (r){
-            case 1:
-                System.out.println("Employee Present");
-                System.out.println("DayWage = " + Wage_HR * Day_HRs);
-                break;
-            case 2:
-                System.out.println("Employee Present");
-                System.out.println("DayWage with Part Time = " + Wage_HR * (Day_HRs + Part_HRs));
-                break;
-            case 0:
-                System.out.println("Employee Absent");
+
+        for (int i =0; i<NumDays; i++){
+            int r = random.nextInt(3);
+            switch (r){
+                case 1:
+                    daywage = Wage_HR * Day_HRs * i;
+                    break;
+                case 2:
+                    daywage = Wage_HR * (Day_HRs + Part_HRs) * i;
+                    break;
+                case 0:
+                    break;
+
+            }
+            monthwage = monthwage + daywage;
         }
+        System.out.println("Month Wage : " + monthwage);
+
+
     }
 }
